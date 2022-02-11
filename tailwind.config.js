86c44d44efbb811823ad3b58fd6285dba8a13a92
Idorenyin Akaininyene <=>
2022-02-11 13:39:53 +0100

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+    content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
