dbb37f69032571ed8a8c8862012560a1cb6ef254
Idorenyin Akaininyene <=>
2022-02-12 01:42:55 +0100

    mobile view for service section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,15 @@
 module.exports = {
     content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            width: {
+                'iw': '15.5vw'
+            },
+
+            borderWidth: {
+                3: '3.5px'
+            }
+        },
     },
     plugins: [],
 }
