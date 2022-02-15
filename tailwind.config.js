5aaab36ae6ccd2aa8b04d72db73564662c5c9129
Idorenyin Akaininyene <=>
2022-02-15 03:07:24 +0100

    mobile view for featured products section on the home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,12 @@ module.exports = {
     theme: {
         extend: {
             width: {
-                'iw': '15.5vw'
+                'iw': '15.5vw',
+                'wi': '96vw'
+            },
+
+            height: {
+                'iv': '56vh'
             },
 
             borderWidth: {
