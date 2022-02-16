44c2cbcff2ea219c7f7a65519d77b33e1baa6282
Idorenyin Akaininyene <=>
2022-02-16 05:09:44 +0100

    mobile nav fixes

---
+++ public/resources/js/main.js
@@ -0,0 +1,10 @@
+  const openMenu = () => {
+      var navItems = document.getElementById('navigations');
+      navItems.style.display = 'block'
+      console.log('l')
+  }
+
+  const closeMenu = () => {
+      var navItems = document.getElementById('navigations');
+      navItems.style.display = 'none';
+  }
